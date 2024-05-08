c84c0245951b6430dbfb590bd86af6124273067d
Kevin Schaper <kevinschaper@gmail.com>
2024-05-08 09:16:04 -0700

    refactored into fewer files, added a guess at the merged schema and insert for gencc

--- sql/load_gencc.sql
+++
@@ -1,3 +0,0 @@
-create or replace table gencc as select * from 'data/gencc.tsv';
-
--- create or replace table exomiser_disease as select * from 'resources/disease.pg';
--- sql/load_mondo_sssom.sql
+++
@@ -1,1 +0,0 @@
-create or replace table sssom as select * from 'data/mondo.sssom.tsv';
