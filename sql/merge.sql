306bb514c57685664d416d4444070759ea3eab69
Kevin Schaper <kevinschaper@gmail.com>
2024-05-14 14:33:56 -0700

    add case statement to map exomiser moi_code to HP mode of inheritance terms

--- sql/merge.sql
+++ sql/merge.sql
@@ -16,7 +16,20 @@ select
     gene_sssom.subject_id as hgnc_id,
     disease_curie as original_disease_id,
     mondo_sssom.subject_id as mondo_id,
-    moi_code as moi_id,
+    -- derived from https://github.com/exomiser/Exomiser/blob/ffb2da103ca54a9de03ceecfc70c8ddfcebba3fd/exomiser-core/src/main/java/org/monarchinitiative/exomiser/core/prioritisers/model/InheritanceMode.java#L38
+    case
+        when moi_code = 'B' then 'exomiser:B' -- autosomal dominant/recessive ?
+        when moi_code = 'D' then 'HP:0000006' -- autosomal dominant
+        when moi_code = 'M' then 'HP:0001427' -- mitochondrial
+        when moi_code = 'P' then 'HP:0010982' -- polygenic
+        when moi_code = 'R' then 'HP:0000007' -- autosomal recessive
+        when moi_code = 'S' then 'exomiser:S' -- somatic ?
+        when moi_code = 'U' then null -- unknown, capture as excplicitly unknown?
+        when moi_code = 'X' then 'HP:0001417'
+        when moi_code = 'XD' then 'HP:0001423'
+        when moi_code = 'XR' then 'HP:0001419'
+        when moi_code = 'Y' then 'HP:0001450'
+        else null end as moi_id,
     'exomiser' as data_aggregator,
     case when disease_curie like 'OMIM:%' then 'omim'
          when disease_curie like 'ORPHA:%' then 'orphanet'
